9d4d238940ed01798d89e62d61c6daa8f782c5a7
Pratik <khadkapratikk10@gmail.com>
2023-12-13 21:20:08 +0545

    add dummy data

---
+++ src/dummyNotes.js
@@ -0,0 +1,29 @@
+const dummyNotes = [
+  {
+    key: 1,
+    title: 'Meeting Agenda',
+    content: 'Discuss project timelines and assign tasks to team members.',
+  },
+  {
+    key: 2,
+    title: 'Grocery List',
+    content: 'Milk, eggs, bread, fruits, and vegetables.',
+  },
+  {
+    key: 3,
+    title: 'Book Recommendations',
+    content: "Check out 'The Alchemist' and 'Sapiens' for your next read.",
+  },
+  {
+    key: 4,
+    title: 'Fitness Plan',
+    content: 'Monday: Cardio, Tuesday: Strength training, Wednesday: Rest day.',
+  },
+  {
+    key: 5,
+    title: 'Travel Bucket List',
+    content: 'Dream destinations: Paris, Tokyo, New York, and Santorini.',
+  },
+];
+
+export default dummyNotes;
